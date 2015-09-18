3d5cd8b86ea813ac0f8831bfe2b7737a748abb07
Fabio Marquesim <marquesim.fabio@gmail.com>
2015-09-18 18:29:25 -0300

    Alteração PDF e index
    Criação do arquivo .inc

--- index.php
+++ index.php
@@ -7,6 +7,26 @@
         <link rel="stylesheet" href="style.css" type="text/css" >
     </head>
     <body>
+        <!--Cabeçalho-->
+        <header>
+        </header>
         
+        <!--Menu-->
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Leia</a></li>
+                <li><a href="#">Assista</a></li>
+                <li><a href="#">Fotos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        
+        <!--Conteudo-->
+        
+        
+        <!--Rodape-->
+        <footer>
+        </footer>
     </body>
 </html>
