02e9a5336f4f23b425bef5d37532db19087c89bb
Fabio Marquesim <marquesim.fabio@gmail.com>
2015-09-18 15:30:56 -0300

    Projeto_Base
    Arquivos base criados.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home - TechLife</title>
+        <meta charset="utf-8" />
+        <meta name="author" content="Tech LifeStyle" />
+        <link rel="stylesheet" href="style.css" type="text/css" >
+    </head>
+    <body>
+        
+    </body>
+</html>
