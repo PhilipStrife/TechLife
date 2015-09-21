1c6a756d5c3d80ed1635ee818b70b5917780109e
Fabio Marquesim <marquesim.fabio@gmail.com>
2015-09-21 17:29:48 -0300

    Adição de comentários e tags bases
    Pequeno css com página index sem conteúdo.

--- index.php
+++ index.php
@@ -15,10 +15,50 @@
         
         
         <!--Conteudo-->
-        
+        <section id="homeOnly">
+            <article class="allArticle">
+                <!--Video Demo Site-->
+                <h2>Vídeo DEMO</h2>
+                <video controls id="trailer">
+                    <source src="https://www.youtube.com/embed/mzPxo7Y6JyA" type="video/demo">
+                </video>
+            </article>
+            <article>
+                <!--Textos-->
+                <h2>Textos</h2>
+                <p>
+                    <!--Texto 01-->
+                </p>
+                <p>
+                    <!--Texto 02-->
+                </p>
+                <p>
+                    <!--Texto 03-->
+                </p>
+            </article>
+            <article>
+                <h2>Vídeos</h2>
+                <!--Videos-->
+                <p>
+                    <!--Video 01-->
+                </p>
+                <p>
+                    <!--Video 02-->
+                </p>
+                <p>
+                    <!--Video 03-->
+                </p>
+            </article>
+            <article>
+                <h2>Fotos</h2>
+                <!--Galeria de Fotos-->
+            </article>
+        </section>
         
         <!--Rodape-->
         <footer>
+            <!--Redes Sociais-->
+            <!--Copyright-->
         </footer>
     </body>
 </html>
